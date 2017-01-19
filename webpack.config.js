8016f62bb446a39f6c24ff0e73017a2ed12ff496
Flonka <flonkation@gmail.com>
2017-01-20 00:24:02 +0200

    build vendor chunk and make use of the clean plugin.

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 // Post-css plugins
 const autoprefixer = require('autoprefixer')
 
 const TARGET = process.env.npm_lifecycle_event
 
+const BUILD_DIR = 'build/'
+
 const common = {
 	resolve: {
 		extensions: ['', '.js', '.jsx', '.scss']
 	},
-
-	entry: './timeit/main',
+	entry: {
+		app: './timeit/main',
+		vendor: ['react', 'react-dom']
+	},
 	output: {
-		path: 'build/',
-		filename: 'app.js'
+		path: BUILD_DIR,
+		filename: 'app.[chunkhash].js'
 	},
 	sassLoader: {
 		includePaths: ['./node_modules/foundation-sites/scss']
@@ -51,18 +56,50 @@ const common = {
 			'styles.[contenthash].css',
 			{allChunks: true}
 		),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+			filename: 'vendor.[chunkhash].js',
+			minChunks: isVendor
+		}),
+		new CleanWebpackPlugin([BUILD_DIR]),
 		new webpack.NoErrorsPlugin()
 	]
 }
 
+function isVendor(module, count) {
+	const userRequest = module.userRequest
+	return userRequest && userRequest.indexOf('node_modules') >= 0
+}
+
+
+const dev = {
+	debug: true,
+	devtool: 'eval-source-map',
+	plugins: [
+		new webpack.NamedModulesPlugin()
+	]
+}
+
+const prod = {
+	devtool: 'source-map'
+}
+
 var buildConfig
 switch (TARGET) {
-	case 'build':
-		buildConfig = merge(common)
+	case 'build-prod':
+		buildConfig = merge.smart(
+			common,
+			prod)
+		break
+	case 'build-dev':
+		buildConfig = merge.smart(
+			common,
+			dev)
 		break
 	case 'start':
-		buildConfig = merge(
+		buildConfig = merge.smart(
 			common,
+			dev,
 			{devServer: {
 				inline: true,
 				context: 'build',
